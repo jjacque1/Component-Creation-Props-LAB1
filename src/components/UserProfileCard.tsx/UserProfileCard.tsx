1186450c4d25b18c104e62d8d60cbbcf3de18f71
jjacque1 <jacksonjacque.jj@gmail.com>
2025-12-02 14:19:39 -0500

    imported the usereprofleprop and created a userprofilecard component that accepts thee interface created in index.ts and returns a div with h2 for user.name, returns user.emal, user.rold, and onclick

--- src/components/UserProfileCard.tsx/UserProfileCard.tsx
+++ src/components/UserProfileCard.tsx/UserProfileCard.tsx
@@ -1,3 +1,37 @@
 import { UserProfileCardProps } from "../types";
 
-export function
+export function UserProfileCard({
+    user,
+    showEmail,
+    showRole,
+    onEdit, 
+    children,
+}: UserProfileCardProps ) {
+    return (
+        <div className="border p-4">
+            <h2>{ user.name }</h2>
+
+            {
+                showEmail && (
+                    <p>{ user.email }</p>
+                )
+            }
+
+            {
+                showRole && (
+                    <p>{ user.role }</p>
+                )
+            }
+
+            {
+                onEdit && (
+                    <button onClick={() => onEdit(user.name)}>
+                        Edit Profile
+                    </button>
+                )
+            }
+
+            {children}
+        </div>
+    )
+}
